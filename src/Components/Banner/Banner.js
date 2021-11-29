7f6da374889fdbd41ad753f4d3ae24dde6cd3ec9
Mahmudul Hasan <17101120@uap-bd.edu>
2021-11-29 22:59:02 +0600

    resource upload

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import photo from '../../Images/profile.jpg'
 import './Banner.css'
+import Aos from 'aos';
+import 'aos/dist/aos.css'
 
 const Banner = () => {
+    useEffect(() => {
+        Aos.init({ duration: 2000 })
+    }, [])
     return (
         <div className='mt-5'>
             <div className="row">
                 <div className="col-md-5 col-sm-12 col-xs-12 ">
-                    <div className='m-1 text-primary d-flex justify-content-end'>
+                    <div className='m-1 text-primary d-flex justify-content-end' data-aos="fade-right"
+                        data-aos-offset="300"
+                        data-aos-easing="ease-in-sine">
                         <img src={photo} alt="" width='250' style={{ borderRadius: '50%', border: '10px solid #C1D3D2' }} />
                     </div>
                 </div>
                 <div className="col-md-7 col-sm-12 col-xs-12">
                     <div className='m-1 mt-2 px-3 d-flex flex-column justify-content-end'>
                         <p className='fs-4 fw-bold' style={{ color: 'gray' }} >Hi,  This is </p>
-                        <h1 className='style-font fw-bolder fs-1'>Mahmudul Hasan</h1>
+                        <div data-aos="zoom-in" >
+                            <h1 className='style-font fs-1' style={{ fontWeight: '900' }}>Mahmudul <span style={{ color: 'blue' }}> Hasan</span></h1>
+                        </div>
+
                         <div>
                             <p style={{ textAlign: 'justify', fontWeight: 500, color: 'gray' }}>I am a Web developer. I prefer to with JavaScript. As a web developer, I normally use React.js as a front-end libray and Node.js as a back-end framework. I also work with Python programming language. Sometimes i used Django as a back-end framework. Artificial Intelligence is another field that interests me.</p>
                         </div>
-                        <div>
+                        <div data-aos="zoom-out">
 
-                            <Button className='primary-button1 my-2'> Download CV</Button>
+                            <a href='https://drive.google.com/file/d/1sgR1WWbYMYY80aDuT5uSY4a3Z30jDj8e/view?usp=sharing' className='primary-button1 my-2 btn'> Download CV</a>
                             <a className='btn secondary-button1 ' href='https://drive.google.com/file/d/1sgR1WWbYMYY80aDuT5uSY4a3Z30jDj8e/view?usp=sharing' > Contact Me</a>
                         </div>
                     </div>
--- src/Components/ProjectSection/ProjectSection.js
+++ src/Components/ProjectSection/ProjectSection.js
@@ -4,6 +4,9 @@ const ProjectSection = () => {
     return (
         <div>
             <div className='mt-5'>
+                <div className='mt-3'>
+                    <h1 className='text-center ' style={{ fontSize: '55px', fontWeight: '900' }}> Proj<span style={{ color: 'blue' }}>ect</span></h1>
+                </div>
                 <div className="row">
                     <div className="col-md-4 col-sm-12 col-xs-12 ">
 
